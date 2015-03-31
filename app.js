e44e2d58ac3efb8a6218cc9dfc897236ba64f8f0
Alf Eaton <hubgit@@users.noreply.github.com>
2015-03-31 12:20:11 +0100

    Allow the XML URL to be specified

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-$(document).one("click", ".lens-article", function(event) {
-	event.preventDefault();
-	var app = new window.Lens({ document_url: this.href });
+var app;
+
+var loadArticle = function(url) {
+	app = new window.Lens({ document_url: url });
 	app.start();
-	window.app = app;
-});
+}
 
-var articles = document.createElement("ul");
-articles.style.margin = "20px";
-document.body.appendChild(articles);
+var matches = location.search.match(/^\?url=(.+?\.xml)/);
 
-$.getJSON("https://peerj.com/articles/index.json?limit=20", function(data) {
-	$.each(data._items, function(key, item) {
-		var article = $("<a/>", { text: item.title, href: item._links.alternate.xml.href });
-		article.addClass("lens-article");
-		$("<li/>").append(article).appendTo(articles);
+if (matches) {
+	loadArticle(matches[1]);
+} else {
+	$(document).one("click", ".lens-article", function(event) {
+		event.preventDefault();
+		loadArticle(this.href);
 	});
-});
+
+	var articles = document.createElement("ul");
+	articles.style.margin = "20px";
+	document.body.appendChild(articles);
+
+	$.getJSON("https://peerj.com/articles/index.json?limit=20", function(data) {
+		$.each(data._items, function(key, item) {
+			var article = $("<a/>", { text: item.title, href: item._links.alternate.xml.href });
+			article.addClass("lens-article");
+			$("<li/>").append(article).appendTo(articles);
+		});
+	});
+}
